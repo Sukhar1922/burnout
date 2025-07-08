119b6d594e8c9f684066f86a5266f07b311e3078
Sukhar1922 <s.a.v.04@ya.ru>
2025-07-08 22:41:14 +0300

    run django bash script

---
+++ run_django.sh
@@ -0,0 +1,13 @@
+SESSION="django_proj"
+
+if ! screen -ls | grep -q "$SESSION"; then
+  echo "Стартуем"
+  screen -dmS "$SESSION" bash -c "
+    source venv/bin/activate;
+    cd server;
+    python3.12 manage.py runserver 0.0.0.0:82;
+    exec bash
+  "
+else
+  echo "Уже есть сессия screen $SESSION"
+fi
